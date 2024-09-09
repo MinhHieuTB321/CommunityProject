acbbaac95d42dc5c8836ed43357220e35d5ffd83
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-09-09 17:15:46 +0700

    fix test

--- WebApi/Controllers/TestsController.cs
+++ WebApi/Controllers/TestsController.cs
@@ -3,40 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    public class ResponseData
+    public class Transaction
     {
-        public string Code { get; set; }
-        public string Desc { get; set; }
-        public bool Success { get; set; }
-        public Data Data { get; set; }
-        public string Signature { get; set; }
+        public int Id { get; set; }
+        public DateTime When { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public decimal CusumBalance { get; set; }
+        public string Tid { get; set; }
+        public string SubAccId { get; set; }
+        public string BankSubAccId { get; set; }
+        public string VirtualAccount { get; set; }
+        public string VirtualAccountName { get; set; }
+        public string CorresponsiveName { get; set; }
+        public string CorresponsiveAccount { get; set; }
+        public string CorresponsiveBankId { get; set; }
+        public string CorresponsiveBankName { get; set; }
     }
 
-    public class Data
+    public class Response
     {
-        public int OrderCode { get; set; }
-        public int Amount { get; set; }
-        public string Description { get; set; }
-        public string AccountNumber { get; set; }
-        public string Reference { get; set; }
-        public DateTime TransactionDateTime { get; set; }
-        public string Currency { get; set; }
-        public string PaymentLinkId { get; set; }
-        public string Code { get; set; }
-        public string Desc { get; set; }
-        public string CounterAccountBankId { get; set; }
-        public string CounterAccountBankName { get; set; }
-        public string CounterAccountName { get; set; }
-        public string CounterAccountNumber { get; set; }
-        public string VirtualAccountName { get; set; }
-        public string VirtualAccountNumber { get; set; }
+        public int Error { get; set; }
+        public Transaction[] Data { get; set; }
     }
     public class TestsController : BaseController
     {
         [HttpPost]
-        public IActionResult Test([FromBody] ResponseData data)
+        public IActionResult Test([FromBody] Response data)
         {
-            return Ok("Oke: \n" + data);
+            return Ok("Oke: \n" + data.Error);
         }
     }
 
