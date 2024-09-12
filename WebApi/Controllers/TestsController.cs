6c14de945fdf85ee2cc04020a96355ad28ecc13c
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-09-12 17:17:31 +0700

    fix test

--- WebApi/Controllers/TestsController.cs
+++ WebApi/Controllers/TestsController.cs
@@ -11,33 +11,31 @@ namespace WebApi.Controllers
     }
     public class RequestData
     {
-        public string Code { get; set; }
-        public string Desc { get; set; }
+        public string Code { get; set; } = default!;
+        public string Desc { get; set; } = default!;
         public bool Success { get; set; }
-        public Data Data { get; set; }
-        public string Signature { get; set; }
+        public Data? Data { get; set; }
+        public string? Signature { get; set; }
     }
 
     public class Data
     {
         public int OrderCode { get; set; }
         public int Amount { get; set; }
-        public string Description { get; set; }
-        public string AccountNumber { get; set; }
-        public string Reference { get; set; }
-        public string TransactionDateTime { get; set; }
-        public string Currency { get; set; }
-        public string PaymentLinkId { get; set; }
-        public string Code { get; set; }
-        public string Desc { get; set; }
-
-        public string CounterAccountBankId { get; set; }
-        public string CounterAccountBankName { get; set; }
-
-        public string CounterAccountName { get; set; }
-        public string CounterAccountNumber { get; set; }
-        public string VirtualAccountName { get; set; }
-        public string VirtualAccountNumber { get; set; }
+        public string? Description { get; set; }
+        public string? AccountNumber { get; set; }
+        public string? Reference { get; set; }
+        public string? TransactionDateTime { get; set; }
+        public string? Currency { get; set; }
+        public string? PaymentLinkId { get; set; }
+        public string? Code { get; set; }
+        public string? Desc { get; set; }
+        public string? CounterAccountBankId { get; set; }
+        public string? CounterAccountBankName { get; set; }
+        public string? CounterAccountName { get; set; }
+        public string? CounterAccountNumber { get; set; }
+        public string? VirtualAccountName { get; set; }
+        public string? VirtualAccountNumber { get; set; }
 
     }
 
@@ -47,28 +45,21 @@ namespace WebApi.Controllers
     }
     public class TestsController : BaseController
     {
-        private readonly ILogger<TestsController> _logger;
 
         private readonly IMongoRepository _repository;
 
-        public TestsController(ILogger<TestsController> logger, IMongoRepository repository)
+        public TestsController(IMongoRepository repository)
         {
-            _logger = logger;
             _repository = repository;
         }
         [HttpPost("/api/webhook-event-handler")]
-        public async Task<IActionResult> Test([FromBody]  request)
+        public async Task<IActionResult> Test([FromBody] RequestData request)
         {
             var data = new WebHook { RequestData = request };
             await _repository.InsertAsync<WebHook>("webHook", data);
 
-            var response = new WebhookResponse
-            {
-                Success = true
-            };
-
             // Return the response
-            return Ok(response);
+            return Ok(new { success = true });
         }
     }
 
