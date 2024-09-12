32bae902f3a3256d98ce2ec44833465ee3929f06
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-09-12 16:06:27 +0700

    fix test

--- WebApi/Controllers/TestsController.cs
+++ WebApi/Controllers/TestsController.cs
@@ -7,56 +7,43 @@ namespace WebApi.Controllers
 {
     public class WebHook : BaseEntity
     {
-        public ResponseData? ResponseData { get; set; }
+        public RequestData? RequestData { get; set; }
     }
-    public class ResponseData
+    public class RequestData
     {
-        // public string Code { get; set; }
-        // public string Desc { get; set; }
-        // public bool Success { get; set; }
-        // public Data Data { get; set; }
-        // public string Signature { get; set; }
-        public string Error { get; set; }
-        public List<Data> Data { get; set; }
+        public string Code { get; set; }
+        public string Desc { get; set; }
+        public bool Success { get; set; }
+        public Data Data { get; set; }
+        public string Signature { get; set; }
     }
 
     public class Data
     {
-        // public int OrderCode { get; set; }
-        // public int Amount { get; set; }
-        // public string Description { get; set; }
-        // public string AccountNumber { get; set; }
-        // public string Reference { get; set; }
-        // public string TransactionDateTime { get; set; }
-        // public string Currency { get; set; }
-        // public string PaymentLinkId { get; set; }
-        // public string Code { get; set; }
-        // public string Desc { get; set; }
-
-        // public string CounterAccountBankId { get; set; }
-        // public string CounterAccountBankName { get; set; }
-
-        // public string CounterAccountName { get; set; }
-        // public string CounterAccountNumber { get; set; }
-        // public string VirtualAccountName { get; set; }
-        // public string VirtualAccountNumber { get; set; }
-        public int Id { get; set; }
-        public string Tid { get; set; }
+        public int OrderCode { get; set; }
+        public int Amount { get; set; }
         public string Description { get; set; }
-        public decimal Amount { get; set; }
-        public decimal CusumBalance { get; set; }
-        public DateTime When { get; set; }
-        public string BankSubAccId { get; set; }
-        public string SubAccId { get; set; }
-        public string BankName { get; set; }
-        public string BankAbbreviation { get; set; }
-        public string VirtualAccount { get; set; }
-        public string VirtualAccountName { get; set; }
-        public string CorresponsiveName { get; set; }
-        public string CorresponsiveAccount { get; set; }
-        public string CorresponsiveBankId { get; set; }
-        public string CorresponsiveBankName { get; set; }
+        public string AccountNumber { get; set; }
+        public string Reference { get; set; }
+        public string TransactionDateTime { get; set; }
+        public string Currency { get; set; }
+        public string PaymentLinkId { get; set; }
+        public string Code { get; set; }
+        public string Desc { get; set; }
 
+        public string CounterAccountBankId { get; set; }
+        public string CounterAccountBankName { get; set; }
+
+        public string CounterAccountName { get; set; }
+        public string CounterAccountNumber { get; set; }
+        public string VirtualAccountName { get; set; }
+        public string VirtualAccountNumber { get; set; }
+
+    }
+
+    public class WebhookResponse
+    {
+        public bool Success { get; set; }
     }
     public class TestsController : BaseController
     {
@@ -70,11 +57,18 @@ namespace WebApi.Controllers
             _repository = repository;
         }
         [HttpPost("/api/webhook-event-handler")]
-        public async Task<IActionResult> Test([FromBody] ResponseData response)
+        public async Task<IActionResult> Test([FromBody] RequestData request)
         {
-            var data = new WebHook { ResponseData = response };
+            var data = new WebHook { RequestData = request };
             await _repository.InsertAsync<WebHook>("webHook", data);
-            return Ok(new { Success = true });
+
+            var response = new WebhookResponse
+            {
+                Success = true
+            };
+
+            // Return the response
+            return Ok(response);
         }
     }
 
