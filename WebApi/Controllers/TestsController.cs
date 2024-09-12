af19945621de02800cbfa591ad273d3f9c81e6f2
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-09-12 11:02:16 +0700

    fix test

--- WebApi/Controllers/TestsController.cs
+++ WebApi/Controllers/TestsController.cs
@@ -23,6 +23,17 @@ namespace WebApi.Controllers
         public int OrderCode { get; set; }
         public int Amount { get; set; }
         public string Description { get; set; }
+        public string AccountNumber { get; set; }
+        public string Reference { get; set; }
+        public string TransactionDateTime { get; set; }
+        public string Currency { get; set; }
+        public string PaymentLinkId { get; set; }
+        public string Code { get; set; }
+        public string Desc { get; set; }
+
+        public string CounterAccountBankId { get; set; }
+        public string CounterAccountBankName { get; set; }
+
         public string CounterAccountName { get; set; }
         public string CounterAccountNumber { get; set; }
         public string VirtualAccountName { get; set; }
